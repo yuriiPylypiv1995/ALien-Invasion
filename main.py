6b0a2f4e07b81981d53213499312033d0dd108dc
Yurii <yra.pilipsv@gmail.com>
2021-12-30 20:14:10 +0200

    Added _check_events and _update_screen methods

--- main.py
+++ main.py
@@ -11,24 +11,30 @@ class AlienInvasion:
         self.settings = Settings()
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
 
-        pygame.display.set_caption("Alian Invasion")
+        pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
 
     def run_game(self):
         """The main cycle of the game"""
         while True:
-            # The events monitoring
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            # Repainting the screen after each cycle iteration
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+    @staticmethod
+    def _check_events():
+        # The events monitoring
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-            # Show the last painted screen
-            pygame.display.flip()
+    def _update_screen(self):
+        # Repainting the screen after each cycle iteration
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # Show the last painted screen
+        pygame.display.flip()
 
 if __name__ == "__main__":
     # Creating the game object and run the game
